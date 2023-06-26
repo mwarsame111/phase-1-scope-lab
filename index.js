f608df6457b8eef5653f5018ec5357e8a06bea22
mohamed warsame <mwarsame111@gmail.com>
2023-06-26 16:13:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+
+}
+
+
+var bestCustomer ;
+function setBestCustomer() {
+    return bestCustomer ='not bob';
+}
+
+
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob";
+}
+
+
+
+const leastFavoriteCustomer = "Nedy"
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "Baha"
+}
+
+
